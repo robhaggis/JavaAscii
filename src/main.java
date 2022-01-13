2e5212b4bb71626384dbf9ced61117e81b654418
robhaggis <robhiggins2001@hotmail.com>
2022-01-13 00:32:23 +0000

    Simple Ascii Text Generator

--- src/main.java
+++ src/main.java
@@ -7,48 +7,58 @@ import java.io.FileWriter;
 import java.io.IOException;
 
 public class main {
-    public static void main(String[] args){
+    public static void main(String[] args) {
 
-        //Load image, scale to 256x256, convert to greyscale
+        final int WIDTH = 256;
+        final int HEIGHT = 256;
+
+        //Load image, scale down, convert to greyscale
         BufferedImage image = null;
-        File input = new File("res/deadpool.jpg");
+        String filename = "deadpool";
+        File input = new File("res/" + filename + ".jpg");
         try {
             image = ImageIO.read(input);
         } catch (IOException e) {
             e.printStackTrace();
         }
-        Image scaled = image.getScaledInstance(256, 256, Image.SCALE_DEFAULT);
-        BufferedImage greyScale = new BufferedImage(256, 256, BufferedImage.TYPE_BYTE_GRAY);
+        Image scaled = image.getScaledInstance(WIDTH, HEIGHT, Image.SCALE_DEFAULT);
+        BufferedImage greyScale = new BufferedImage(WIDTH, HEIGHT, BufferedImage.TYPE_BYTE_GRAY);
         Graphics g = greyScale.getGraphics();
         g.drawImage(scaled, 0, 0, null);
 
-        String output = "";
-        //Assign each value an ascii character and append to string
-        for(int x=0;x<256;x++){
-            for(int y=0;y<256;y++){
-                Color col = new Color(greyScale.getRGB(y,x));
-                int greyVal = col.getRed()>>5;
-                switch(greyVal){
-                    case 0: output = output.concat("@");break;
-                    case 1: output = output.concat("%");break;
-                    case 2: output = output.concat("#");break;
-                    case 3: output = output.concat("*");break;
-                    case 4: output = output.concat("+");break;
-                    case 5: output = output.concat("=");break;
-                    case 6: output = output.concat(":");break;
-                    case 7: output = output.concat(" ");break;
-                }
+        //Ascii Character Set
+        String asciiOrderedByDensity = "   ,:;Il!i~+_-?][}{1)(|/tfjrxnuvczXYUJCLQ0OZmwqpdbkhao*#MW&8%B@$";
+        String asciiReversed = new StringBuilder(asciiOrderedByDensity).reverse().toString();
+
+        String outputNormal = "";
+        String outputInverted= "";
+
+        //Bit shift 256-bit greyscale value right by 2 as we have 64 ascii values to choose from
+        for (int y = 0; y < HEIGHT; y++) {
+            for (int x = 0; x < WIDTH; x++) {
+                Color col = new Color(greyScale.getRGB(x, y));
+                int greyVal = col.getRed() >> 2;
+                outputNormal = outputNormal.concat(asciiOrderedByDensity.substring(greyVal, greyVal+1));
+                outputInverted = outputInverted.concat(asciiReversed.substring(greyVal, greyVal+1));
             }
-            output = output.concat("\n");
+            outputNormal = outputNormal.concat("\n");
+            outputInverted = outputInverted.concat("\n");
         }
 
         //Write string to file with timestamp to prevent overwriting
         BufferedWriter writer = null;
         try {
-            writer = new BufferedWriter(new FileWriter("res/output" + (System.currentTimeMillis()/1000) +  ".txt"));
-            writer.write(output);
-        } catch (IOException e) {
+            writer = new BufferedWriter(new FileWriter("res/" + filename +  (System.currentTimeMillis() / 1000) + ".txt"));
+            writer.write(outputInverted);
+            writer = new BufferedWriter(new FileWriter("res/" + filename + "INVERTED" +   (System.currentTimeMillis() / 1000) + ".txt"));
+            writer.write(outputNormal);
+        } catch (
+                IOException e) {
             e.printStackTrace();
         }
     }
 }
+
+
+
+
